a0bbf85ad740086e61a8767be6ea59218906f7f4
troyan911 <troyan911@gmail.com>
2024-06-15 18:19:28 +0300

    weater app

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,64 @@
 
 <template>
-  <input type="text" v-model="userName" placeholder="name">
-  <input type="email" v-model="userEmail" name="email" placeholder="email">
-  <input type="password" v-model="userPass" name="password" placeholder="password">
-  <input type="button" @click="sendData()" value="Send">
-  
-    <div v-if="users.length == 0" className="user">User list is empty</div>
-    <div v-else-if="users.length == 1" className="user">User has one element</div>
-    <div v-else className="user">User has more than one element</div>
+<div class="wrapper">
+  <h1>Weather app</h1>
+  <p>Get weather in {{ city == "" ? " your city" : cityName }}</p>
+  <input type="text" v-model="city" placeholder="Type your city">
+    <button v-if="city != ''" @click="getWeather()">See weather</button>
+    <button disabled v-else>Input city name</button>
+    <p class="error">{{ error }}</p>
 
-  <User v-for="(el, index) in users" :key="index" :user="el" :index="index" :deleteUser="deleteUser" />
-
-  <p className="error">{{ error }}</p>
-
-
-  <p>{{ users }}</p>
+    <div v-if="info != null">
+      <p > {{ this.info }}</p>
+      <p > {{ weather }}</p>
+      <p > {{ showTemp }}</p>
+      <p > {{ showTempMin }}</p>
+      <p > {{ showTempMax }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import User from './components/User.vue';
+import axios from 'axios'
 
 export default {
-  components: { User },
   data() { 
     return {
-      users: [],
-      error: '',
-      userName: '',
-      userEmail: '',
-      userPass: ''
+      city: "",
+      error: "",
+      info: null
+    }
+  },
+  computed: {
+    cityName() {
+      return "\"" + this.city + "\"";
+    },
+    showTemp() {
+      return "Temperature: " + this.info.main.temp;
+    },
+    showTempMin() {
+      return "Temperature min: " + this.info.main.temp_min;
+    },
+    showTempMax() {
+      return "Temperature max: " + this.info.main.temp_max;
+    },
+    weather() { 
+      return "Weather: " + this.info.weather[0].main;
     }
   }, 
   methods: {
-    sendData(val) { 
-      if (this.userName == '') {
-        this.error = 'empty name';
-        return;
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Type for city name more than 1 symbols";
+        return false;
       }
-      else if (this.userEmail == '') {
-        this.error = 'empty email';
-        return;
-      }
-      else if (this.userPass == '') {
-        this.error = 'empty pass';
-        return;
-      }
+      this.error = "";
 
-      this.error = '';
+      const appid = '557722ec3842bae278742cdcfd76ebf9';
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${appid}`
+      axios.get(url)
+        .then(res => this.info = res.data)
 
-      this.users.push({
-        name: this.userName,
-        email: this.userEmail,
-        pass: this.userPass,
-      })
-    },
-    deleteUser(index) {
-      this.users.splice(index, 1);
     }
   }
 }
@@ -64,40 +66,59 @@ export default {
 
 <style scoped>
 
-  input {
-    display: block;
-    margin-bottom: 10px;
-    border-radius: 3px;
-    border: 1px solid silver;
-    outline: none;
-    padding: 10px 15px;
-    background: #fafafa;
-    color: #333;
-  }
+.error {
+  color: #e77700;
+}
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background: #1cb700;
+  padding: 20px;
+  text-align: center;
+  color: #fff;
+}
 
-  button {
-    border: 0;
-    border-radius: 5px;
-    outline: none;
-    padding: 10px 15px;
-    background-color: #6cd760;
-    color: #167f3d;
-    font-weight: bold;
-    cursor: pointer;
-    transition: transform 500ms ease;
-  }
+.wrapper h1 {
+  margin-top: 50px;
+}
 
-  button:hover {
-    transform: translateY(-5px);
-  }
+.wrapper p {
+  margin-top: 20px;
+}
 
-  .user {
-    width: 500px;
-    margin-top: 20px;
-    border: 1px solid silver;
-    background: #e3e3e3;
-    color: #222;
-    padding: 20px;
-    border-radius: 5px;
-  }
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid #004fb7;
+  color: #fcfcfc;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;  
+}
+
+.wrapper input:focus {
+  border-bottom-color: #fbdf0b;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #fff;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:disabled {
+  background: #746027;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
+}
+
 </style>
